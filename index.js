ffdac8b197660fff4bdc0652fd2381c14b544079
Athallah Dzaki <discordgg5@gmail.com>
2020-11-15 15:10:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const DownloadYTFile = require('yt-dl-playlist')
- 
-const downloader = new DownloadYTFile({ 
-  outputPath: process.cwd(),
-  ffmpegPath: './ffmpeg/bin/ffmpeg.exe',
-  maxParallelDownload: 10,
-  fileNameGenerator: (videoTitle) => {
-    return 'a-new-file-name.mp3'
-  }
-})
+var Downloader = require("./downloader");
+var dl = new Downloader();
+
+dl.getMP3({videoId: "Vhd6Kc4TZls", name: "Cold Funk - Funkorama.mp3"}, function(err,res){
+    if(err)
+        throw err;
+    else{
+        console.log("Song "+ i + " was downloaded: " + res.file);
+    }
+});
