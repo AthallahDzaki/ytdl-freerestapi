706a9fed6ec5d4815bc78f434d3a9520e75f8b50
Athallah Dzaki <discordgg5@gmail.com>
2020-11-15 16:27:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ require('dotenv').config()
  * Get port from environment and store in Express.
  */
 
-app.set('port', process.env.PORT);
-console.log('your app run on port'+process.env.PORT);
+app.set('port', process.env.PORT || 8080);
+console.log('your app run on port '+process.env.PORT);
