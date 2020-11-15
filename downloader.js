006ef468ee69ab676d05fcb79fda27a8dacd45b7
Athallah Dzaki <discordgg5@gmail.com>
2020-11-15 15:24:52 +0700

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -10,7 +10,7 @@ var Downloader = function() {
         "outputPath": "./output/",    // Output file location (default: the home directory)
         "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
         "queueParallelism": 2,                  // Download parallelism (default: 1)
-        "progressTimeout": 2000                 // Interval in ms for the progress reports (default: 1000)
+        "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
         "outputOptions" : ["-af", "silenceremove=1:0:-50dB"] // Additional output options passend to ffmpeg
     });
  
