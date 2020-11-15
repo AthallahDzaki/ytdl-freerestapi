2c599f3e5024b754a1ce822dbc21ad6a94e657e9
Athallah Dzaki <discordgg5@gmail.com>
2020-11-15 12:06:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET','POST'])
 def yta():
 	if request.args.get('url'):
-		if request.args.get('v2') == true
+		if request.args.get('v2')
 		try:
 			url = request.args.get('url').replace('[','').replace(']','')
 			yta = post('https://www.y2mate.com/mates/en60/analyze/ajax',data={'url':url,'q_auto':'0','ajax':'1'}).json()
@@ -35,37 +35,6 @@ def yta():
 				'error': '[❗] Terjadi kesalahan mungkin link yang anda kirim tidak valid!'
 			}
 		else
-		try:
-			url = request.args.get('url').replace('[','').replace(']','')
-			yta = post('https://www.y2mate.com/mates/en60/analyze/ajax',data={'url':url,'q_auto':'0','ajax':'1'}).json()
-			yaha = bs(yta['result'], 'html.parser').findAll('td')
-			filesize = yaha[len(yaha)-10].text
-			id = re.findall('var k__id = "(.*?)"', yta['result'])
-			thumb = bs(yta['result'], 'html.parser').find('img')['src']
-			title = bs(yta['result'], 'html.parser').find('b').text
-			dl_link = bs(post('https://www.y2mate.com/mates/en60/convert',data={'type':url.split('/')[2],'_id':id[0],'v_id':url.split('/')[3],'ajax':'1','token':'','ftype':'mp3','fquality':'128'}).json()['result'],'html.parser').find('a')['href']
-			return {
-				'status': 200,
-				'title': title,
-				'thumb': thumb,
-				'filesize': filesize,
-				'result': dl_link,
-				'ext': 'mp3'
-			}
-		except Exception as e:
-			print('Error : %s' % e)
-			return {
-				'status': False,
-				'error': '[❗] Terjadi kesalahan mungkin link yang anda kirim tidak valid!'
-			}
-	else:
-		return {
-			'status': False,
-			'msg': '[!] Masukkan parameter url'
-		}
-@app.route('/v2', methods=['GET','POST'])
-def yta():
-	if request.args.get('url'):
 		try:
 			url = request.args.get('url').replace('[','').replace(']','')
 			yta = post('https://www.y2mate.com/mates/en60/analyze/ajax',data={'url':url,'q_auto':'0','ajax':'1'}).json()
