9197fd61365e1802ec5c88523e40711452617f47
Athallah Dzaki <discordgg5@gmail.com>
2020-11-15 15:15:54 +0700

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -6,8 +6,8 @@ var Downloader = function() {
     
     //Configure YoutubeMp3Downloader with your settings
     self.YD = new YoutubeMp3Downloader({
-        "ffmpegPath": "/path/to/ffmpeg",        // FFmpeg binary location
-        "outputPath": "/path/to/mp3/folder",    // Output file location (default: the home directory)
+        "ffmpegPath": "/app/vendor/ffmpeg/ffmpeg",        // FFmpeg binary location
+        "outputPath": "./output/",    // Output file location (default: the home directory)
         "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
         "queueParallelism": 2,                  // Download parallelism (default: 1)
         "progressTimeout": 2000                 // Interval in ms for the progress reports (default: 1000)
