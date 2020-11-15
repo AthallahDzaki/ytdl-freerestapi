240edfeeebe9f823de1e7439b222ea44f5b955d9
Athallah Dzaki <discordgg5@gmail.com>
2020-11-15 12:01:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET','POST'])
-return "'status': False,'msg': '[!] Gunakan /v1 atau /v2 untuk convert'"
+return "Gunakan /v1 atau /v2 untuk convert youtube Apabila V1 Tidak Bisa Gunakan V2"
 
 @app.route('/v1', methods=['GET','POST'])
 def yta():
